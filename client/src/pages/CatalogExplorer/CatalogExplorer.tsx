0ae938e54ef5d487466e6fa637afab043642cbe8
Darian ESquivel <darianesquivelf@gmail.com>
2023-03-01 15:07:25 -0300

    added succes, loading and error

--- client/src/pages/CatalogExplorer/CatalogExplorer.tsx
+++ client/src/pages/CatalogExplorer/CatalogExplorer.tsx
@@ -5,6 +5,10 @@ import CatalogCard from "../../components/CatalogCard/CatalogCard";
 import CatalogCreator from "../../components/CatalogCard/CatalogCreator";
 // MUI
 import { makeStyles } from "@material-ui/core/styles";
+import { CircularProgress, IconButton, Typography } from "@material-ui/core";
+import { ReplayOutlined } from "@material-ui/icons";
+
+import { useHistory } from "react-router";
 
 type TcatalogCard = {
   id: string;
@@ -15,7 +19,7 @@ type TcatalogCard = {
   productCount: number;
   className?: any;
 };
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   gridContainer: {
     display: "grid",
     gridTemplateColumns: "repeat(auto-fit, minmax( 290px, 1fr))",
@@ -26,33 +30,73 @@ const useStyles = makeStyles(() => ({
     gridTemplateColumns: "repeat(5, 1fr)",
     gap: "16px",
   },
+  loading: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
+  error: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 }));
 
 const CatalogExplorer = () => {
   const classes = useStyles();
   const { data: catalogs, status } = useQuery(["catalogs"], getAllCatalogs);
+  const history = useHistory();
+
+  const handleReLoad = () => {
+    history.push("/catalogs");
+  };
+  console.log("status", status);
   return (
-    <div
-      className={
-        !catalogs || catalogs?.length < 5
-          ? classes.containerNoData
-          : classes.gridContainer
-      }
-    >
-      <CatalogCreator />
-      {status === "success" &&
-        catalogs.map((catalog: TcatalogCard) => {
-          return (
-            <CatalogCard
-              key={catalog.id}
-              id={catalog.id}
-              name={catalog.name}
-              createdAt={catalog["created_at"]}
-              productCount={catalog.productCount}
-              products={catalog.products}
-            />
-          );
-        })}
+    <div>
+      {status === "loading" ? (
+        <div className={classes.loading}>
+          <CircularProgress />
+        </div>
+      ) : null}
+
+      {status === "success" ? (
+        <div
+          className={
+            !catalogs || catalogs?.length < 5
+              ? classes.containerNoData
+              : classes.gridContainer
+          }
+        >
+          <CatalogCreator />
+          {catalogs.map((catalog: TcatalogCard) => {
+            return (
+              <CatalogCard
+                key={catalog.id}
+                id={catalog.id}
+                name={catalog.name}
+                createdAt={catalog["created_at"]}
+                productCount={catalog.productCount}
+                products={catalog.products}
+              />
+            );
+          })}
+        </div>
+      ) : null}
+
+      {status === "error" ? (
+        <div className={classes.error}>
+          <Typography>an error has occurred</Typography>
+          <IconButton
+            onClick={handleReLoad}
+            color="primary"
+            aria-label="reload"
+          >
+            <ReplayOutlined />
+          </IconButton>
+        </div>
+      ) : null}
     </div>
   );
 };
