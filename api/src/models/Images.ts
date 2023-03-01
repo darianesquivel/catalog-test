a488f6840f13aa97e17f6f9449318369e414cbda
Lucas Ferreira <81488669+lucasdevferreira@users.noreply.github.com>
2023-03-01 13:57:24 -0300

    Model images created

---
+++ api/src/models/Images.ts
@@ -0,0 +1,31 @@
+import { DataTypes, literal } from "sequelize";
+
+module.exports = (sequelize: any) => {
+  // defino el modelo
+  sequelize.define(
+    "images",
+    {
+      id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        defaultValue: literal("gen_random_uuid()"),
+        allowNull: true,
+        unique: true,
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      product_id: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaulValue: new Date(),
+        allowNull: true,
+      },
+    },
+    { timestamps: false }
+  );
+};
