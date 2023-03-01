47acd568d193adcdf5617512b48126f68bdb9880
Darian ESquivel <darianesquivelf@gmail.com>
2023-03-01 10:37:20 -0300

    added mutate and alert

--- client/src/components/AddProducts/AddProducts.tsx
+++ client/src/components/AddProducts/AddProducts.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import addProducts from "../../api/addProducts";
+import Papa from "papaparse";
+import { useStore } from "../../pages/DrawerAppbar/DrawerAppbar";
+import { useMutateHook } from "../../hooks";
+
+//MUI
 import {
   Button,
   CircularProgress,
-  makeStyles,
+  Dialog,
+  IconButton,
   Paper,
   Table,
   TableBody,
@@ -11,18 +20,18 @@ import {
   TableRow,
   Typography,
 } from "@material-ui/core";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import { ReplayOutlined } from "@material-ui/icons";
 
-import addProducts from "../../api/addProducts";
-import { useHistory, useParams } from "react-router-dom";
-
+//FA
 import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import Papa from "papaparse";
-import { useEffect, useState } from "react";
-import { useStore } from "../../pages/DrawerAppbar/DrawerAppbar";
-import { useMutateHook } from "../../hooks";
+//STYLES
+import useStyles from "./styles";
+
+import CustomDialog from "../CustomDialog/CustomDialog";
+import CustomAlert from "../Alert/CustomAlert";
 
 const columns: GridColDef[] = [
   {
@@ -36,141 +45,57 @@ const columns: GridColDef[] = [
   { field: "description", headerName: "Description", width: 150 },
 ];
 
-const useStyles = makeStyles((theme) => ({
-  table: {
-    width: "100%",
-  },
-  tableHead: {
-    height: 60,
-  },
-  tableFooter: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "center",
-    padding: "10px",
-    gap: "10px",
-  },
-  button: {
-    borderRadius: "8px",
-  },
-  tableData: {
-    height: 600,
-  },
-  icons: {
-    marginRight: "5px",
-  },
-  inputContainer: {
-    width: "100%",
-    height: 500,
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    padding: "20px",
-    borderRadius: "8px",
-    boxSizing: "border-box",
-  },
-  labelInput: {
-    width: "100%",
-    height: 450,
-
-    border: "1px dashed #e6e6e6",
-    borderRadius: "8px",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: theme.palette.background.default,
-    transition: "0.3s",
-    "&:hover": {
-      backgroundColor: theme.palette.grey[200],
-      border: "1px dashed #6A5DF9",
-    },
-    cursor: "pointer",
-  },
-  buttonInput: {
-    width: "100%",
-    height: "100%",
-  },
-  typography: {
-    fontSize: "13px",
-    textTransform: "none",
-    color: "grey",
-  },
-  typographyBold: {
-    fontSize: "16px",
-    fontWeight: "bold",
-    textTransform: "none",
-  },
-  image: {
-    width: "150px",
-  },
-}));
-
 export default function AddProducts() {
   const [data, setData] = useState([]);
-  const [viewData, setViewData] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [preview, setPreview] = useState(false);
+  const [upload, setUpload] = useState(true);
   const history = useHistory();
   const classes = useStyles();
-  // to do : change for catalog_id
-  const { id: catalog_id } = useParams<{ id: string }>();
-  const {
-    mutate,
-    isLoading,
-    isSuccess,
-    data: mutateData,
-    isError,
-  } = useMutateHook(() => addProducts(catalog_id, data));
-
-  console.log({
-    isLoading,
-    isSuccess,
-    mutateData,
-    isError,
-  });
+  const { id: catalogId } = useParams<{ id: string }>();
+  const { mutate, isLoading, isSuccess, isError } = useMutateHook(() =>
+    addProducts(catalogId, data)
+  );
 
   const { setSectionInfo } = useStore();
   useEffect(() => () => setSectionInfo(""), [setSectionInfo]);
 
   const handleFile = (e: any) => {
-    setLoading(true);
     const file = e.target.files[0];
     Papa.parse(file, {
       header: true,
       skipEmptyLines: true,
       complete: function (result) {
         const csvData: any = result.data;
+
         const sanitizedData = csvData
           .map((obj: any) => {
             const { id, description, title, image } = obj || {};
-            return { id, description, title, image };
+            return { id, description, title, image, catalog_id: catalogId };
           })
           .filter((obj: any) => obj.description && obj.title && obj.image);
-
+        setUpload(false);
         setData(sanitizedData);
-        setViewData(true);
-        setLoading(false);
+        setPreview(true);
       },
     });
   };
 
   const handleCancel = () => {
-    setViewData(false);
+    setPreview(false);
+    setUpload(true);
     setData([]);
   };
 
   const handleSubmit = async () => {
-    // the catalogId is added to the products uploaded via CSV
-    const fulldata: any = data.map((product: any) => ({
-      id: product.id,
-      title: product.title,
-      description: product.description,
-      image: product.image,
-      catalog_id: catalog_id,
-    }));
     mutate();
-    // setViewData(false);
-    // history.push(`/catalogs/${catalog_id}`);
+  };
+
+  const handleReLoad = () => {
+    history.go(0);
+  };
+
+  const handleIsSuccess = () => {
+    history.push(`/catalogs/${catalogId}`);
   };
 
   return (
@@ -187,39 +112,72 @@ export default function AddProducts() {
             </TableCell>
           </TableRow>
         </TableHead>
-        {!viewData ? (
-          loading ? (
-            <div className={classes.inputContainer}>
-              <CircularProgress />
-            </div>
-          ) : (
-            <div className={classes.inputContainer}>
-              <input
-                accept={".csv"}
-                id="contained-button-file"
-                type="file"
-                hidden
-                onChange={handleFile}
+
+        {upload ? (
+          <div className={classes.inputContainer}>
+            <input
+              accept={".csv"}
+              id="contained-button-file"
+              type="file"
+              hidden
+              onChange={handleFile}
+            />
+            <label
+              className={classes.labelInput}
+              htmlFor="contained-button-file"
+            >
+              <img
+                src="https://duploservices-prod01-public2-415703579972.s3.amazonaws.com/scale-illustration-74a56dd7b4daa3127c4605c7475d1b10.png"
+                alt=""
+                className={classes.image}
               />
-              <label
-                className={classes.labelInput}
-                htmlFor="contained-button-file"
-              >
-                <img
-                  src="https://duploservices-prod01-public2-415703579972.s3.amazonaws.com/scale-illustration-74a56dd7b4daa3127c4605c7475d1b10.png"
-                  alt=""
-                  className={classes.image}
-                />
-                <Typography className={classes.typographyBold}>
-                  What data do you wish to import?
-                </Typography>
-                <Typography className={classes.typography}>
-                  Upload a CSV or Excel file to start the import process.
-                </Typography>
-              </label>
-            </div>
-          )
-        ) : (
+              <Typography className={classes.typographyBold}>
+                What data do you wish to import?
+              </Typography>
+              <Typography className={classes.typography}>
+                Upload a CSV or Excel file to start the import process.
+              </Typography>
+            </label>
+          </div>
+        ) : null}
+
+        {isError ? (
+          <div className={classes.error}>
+            <Typography>an error has occurred</Typography>
+            <IconButton
+              onClick={handleReLoad}
+              color="primary"
+              aria-label="reload"
+            >
+              <ReplayOutlined />
+            </IconButton>
+          </div>
+        ) : null}
+
+        {isLoading ? (
+          <div className={classes.loading}>
+            <Typography> Loading </Typography>
+            <CircularProgress />
+          </div>
+        ) : null}
+
+        {isSuccess ? (
+          <Dialog
+            open={isSuccess}
+            onClose={handleIsSuccess}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            <CustomAlert
+              alertType="success"
+              message={`Products were successfully loaded`}
+              closeIcon={true}
+              onClose={handleIsSuccess}
+            />
+          </Dialog>
+        ) : null}
+
+        {preview && !isError && !isLoading && !isSuccess ? (
           <TableBody className={classes.tableData}>
             <TableRow>
               <TableCell align="center">
@@ -236,12 +194,10 @@ export default function AddProducts() {
               </TableCell>
             </TableRow>
           </TableBody>
-        )}
-
-        {/* Reder result table */}
+        ) : null}
 
         <TableHead className={classes.tableFooter}>
-          {viewData ? (
+          {preview && !isError && !isLoading && !isSuccess ? (
             <>
               <Button
                 variant="contained"
