5c5fdf7e90b0f082986669c0f7bf9fd17e7c8159
Darian ESquivel <darianesquivelf@gmail.com>
2023-03-02 12:42:15 -0300

    added theme provider and edit cards styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,24 @@ import axios from "axios";
 //component
 import DrawerAppbar from "./pages/DrawerAppbar/DrawerAppbar";
 
+import { createTheme, ThemeProvider } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6A5DF9",
+    },
+  },
+});
+
 axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
-  return <DrawerAppbar />;
+  return (
+    <ThemeProvider theme={theme}>
+      <DrawerAppbar />
+    </ThemeProvider>
+  );
 }
 
 export default App;
