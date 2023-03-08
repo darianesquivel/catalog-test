7d39c857553f6eaab4d9bbcb33f1ba2560a91d35
Darian ESquivel <darianesquivelf@gmail.com>
2023-03-08 17:50:38 -0300

    normalized styles of buttons

--- client/src/components/CustomDialog/CustomDialog.tsx
+++ client/src/components/CustomDialog/CustomDialog.tsx
@@ -3,46 +3,11 @@ import CustomAlert from "../Alert/CustomAlert";
 
 // MUI
 import { Button, CircularProgress } from "@material-ui/core";
-import { makeStyles, Theme } from "@material-ui/core/styles";
 import { Dialog, DialogActions, DialogContent } from "@material-ui/core";
 import { useMutateHook } from "../../hooks";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  dialog: {},
-  container: {
-    minWidth: "500px",
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    gap: "20px",
-    padding: theme.spacing(2),
-  },
-  childrenBox: {},
-  button: {
-    width: "40px",
-    alignSelf: "flex-end",
-    objectFit: "contain",
-  },
-  cancelButton: {
-    color: "rgb(43, 153, 216)",
-    background: "rgba(43, 153, 216, 0.2)",
-  },
-  buttonWrapper: {
-    width: "100%",
-    display: "flex",
-    alignItems: "center",
-    minWidth: "70px",
-    justifyContent: "flex-end",
-    gap: "10px",
-    margin: 0,
-  },
-  buttonProgress: {
-    color: "green[500]",
-    // position: "absolute",
-  },
-  dialogActions: {},
-}));
+// STYLES
+import useStyles from "./styles";
 
 type Tprops = {
   onModalChange: () => void;
@@ -83,7 +48,6 @@ const CustomDialog = ({
       aria-labelledby="simple-modal-title"
       aria-describedby="simple-modal-description"
       className={classes.dialog}
-      // fullWidth={true}
     >
       {isSuccess ? (
         <CustomAlert
@@ -123,13 +87,19 @@ const CustomDialog = ({
                 />
               ) : (
                 <Button
-                  className={`${classes.button}`}
                   variant="contained"
                   color="primary"
                   size="small"
                   onClick={handleSubmit}
+                  className={
+                    children[1]?.props?.alertType === "error"
+                      ? classes.buttonError
+                      : classes.button
+                  }
                 >
-                  Accept
+                  {children[1]?.props?.alertType === "error"
+                    ? "Delete"
+                    : "Duplicate"}
                 </Button>
               )}
             </div>
