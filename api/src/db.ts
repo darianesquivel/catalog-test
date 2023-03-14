adc077e1de91142bf39327be768447d01d6ffdd6
lucasdevferreira <lucasferreiraok@outlook.com>
2023-03-14 12:00:42 -0300

    rootDir and logging process.env

--- api/src/db.ts
+++ api/src/db.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 require("dotenv").config();
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
-
+console.log("process. env ----->", process.env)
 const localhost = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   {
