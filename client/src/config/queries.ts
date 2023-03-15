bfd9e5e276eb8e6b1f5eda1257983d8f20f8a2be
Lucas Ferreira <81488669+lucasdevferreira@users.noreply.github.com>
2023-03-15 17:21:56 -0300

    added useSingleCatalog query fn in productList

--- client/src/config/queries.ts
+++ client/src/config/queries.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import getAllCatalogs from "../api/getAllCatalogs";
+import getCatalogById from "../api/getCatalogById";
 import getFilteredCatalogs from "../api/getFilteredCatalogs";
 
 export const useCatalogsQuery = (term?: string) => {
@@ -10,4 +11,11 @@ export const useCatalogsQuery = (term?: string) => {
       return getAllCatalogs();
     }
   });
+};
+
+export const useSingleCatalogQuery = (
+  queryKeys: string[],
+  catalogId: string
+) => {
+  return useQuery(queryKeys, () => getCatalogById(catalogId));
 };
