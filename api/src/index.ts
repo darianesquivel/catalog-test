e96cf51c8143002c414750f38453c1bea77eb475
Lucas Ferreira <81488669+lucasdevferreira@users.noreply.github.com>
2023-03-15 13:08:03 -0300

    ramoving innecesary headers

--- api/src/index.ts
+++ api/src/index.ts
@@ -15,15 +15,17 @@ const options: cors.CorsOptions = {
 server.use(cors(options));
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from. * -> autoriza los requests de cualquier puerto
 
-  res.header("Access-Control-Allow-Credentials", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, *"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+server.use((req, res, next) => {
+  console.log("server use luego de los cors-------> ");
+  // res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from. * -> autoriza los requests de cualquier puerto
+
+  // res.header("Access-Control-Allow-Credentials", "*");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, *"
+  // );
+  // res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 const PORT = process.env.PORT || 3001;
