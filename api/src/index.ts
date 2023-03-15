1137240e86187ffbb888af4011684bd6f27b8672
Lucas Ferreira <81488669+lucasdevferreira@users.noreply.github.com>
2023-03-15 10:51:03 -0300

    added cors library

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,19 @@
 import database from "./db";
 import express from "express";
+import cors from "cors";
 import routes from "./routes";
 
 const server = express();
 
+const allowedOrigins = ["https://catalogtest.vercel.app"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+server.use(cors(options));
 server.use(express.urlencoded({ extended: true, limit: "50mb" }));
 server.use(express.json({ limit: "50mb" }));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://catalogtest.vercel.app"); // update to match the domain you will make the request from. * -> autoriza los requests de cualquier puerto
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from. * -> autoriza los requests de cualquier puerto
 
   res.header("Access-Control-Allow-Credentials", "*");
   res.header(
