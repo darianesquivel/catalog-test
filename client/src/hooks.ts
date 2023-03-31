3b4c6c59bd7d523958188394f38d3f0730c354f9
Lucas Ferreira <81488669+lucasdevferreira@users.noreply.github.com>
2023-03-31 10:56:24 -0300

    useCopyToClipboard hook created

--- client/src/hooks.ts
+++ client/src/hooks.ts
@@ -1,5 +1,30 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation } from '@tanstack/react-query';
+import { useState, useEffect } from 'react';
+
+type CopyToClipProps = [isCopied: boolean, copyToClipboard: (text: string) => Promise<any>];
 
 export function useMutateHook(apiFunction: () => any) {
-  return useMutation(apiFunction);
+   return useMutation(apiFunction);
+}
+
+export function useCopyToClipboard() {
+   const [isCopied, setIsCopied] = useState(false);
+
+   useEffect(() => {
+      if (isCopied) {
+         const timeoutId = setTimeout(() => {
+            setIsCopied(false);
+         }, 2000);
+
+         return () => {
+            clearTimeout(timeoutId);
+         };
+      }
+   }, [isCopied]);
+
+   const copyToClipboard: (text: string) => void = (text = '') => {
+      return navigator.clipboard.writeText(text).then(() => setIsCopied(true));
+   };
+
+   return [isCopied, copyToClipboard] as CopyToClipProps;
 }
